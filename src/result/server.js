1f4e2735e8dc5f62411503bb42e973a62a0041e2
Sohan Wadalkar <sohan.wadalkar@gmail.com>
2024-05-01 16:32:17 +0530

    commit1234

--- src/result/server.js
+++ src/result/server.js
@@ -67,7 +67,7 @@ app.use(cookieParser());
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/views'));
 
-app.get('/', function (req, res) {
+app.get('/result', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
